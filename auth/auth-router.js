eb3729a67ab913f37e3ecd671975e774f9d9ba1e
Eddy <edward.gonzalez9708@gmail.com>
2019-10-17 17:41:24 -0400

    Completed guided demo solution for web authentication part 3.

---
+++ auth/auth-router.js
@@ -0,0 +1,58 @@
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const Users = require('../users/users-model.js');
+const secrets = require('../config/secret.js');
+
+// for endpoints beginning with /api/auth
+router.post('/register', (req, res) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.post('/login', (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user)
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          token
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id, // sub
+    username: user.username,
+    // ...other data
+  }
+
+  const options = {
+    expiresIn: '8h'
+  }
+
+  return jwt.sign(payload, secrets.jwtSecret, options);
+}
+
+module.exports = router;
